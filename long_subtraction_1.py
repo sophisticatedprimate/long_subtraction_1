a19b95c2985346091967fbf42f7d21b7538eb126
SophisticatedPrimate <admin@sophisticatedprimate.com>
2022-09-19 18:10:57 -0500

    Add files via upload

---
+++ long_subtraction_1.py
@@ -0,0 +1,181 @@
+"""
+Check out the official python docs to learn more about python programming!
+https://docs.python.org/3/
+
+Differences are found in python by using the subtraction operator (-), in which two values on either side are summed.
+Basic usage is as follows:
+    print(5 - 3) 
+    sum = addendA - addendB
+    difference = positive - (-negative)    ~effectively is the same as addition
+Subtraction of whole numbers are pretty straight forward, to learn more about how python (and essentially all calculators) perform subtraction internally follow these links:
+    https://www.cs.cornell.edu/~tomf/notes/cps104/twoscomp.html
+    https://www.cs.uaf.edu/2015/fall/cs301/lecture/10_05_bitwise.html
+    https://faculty.etsu.edu/tarnoff/ntes2150/logic/bitwise.htm
+    lookup terms: two's complement, x86 assembly, ARM assembly
+    NOTE: you don't need to learn to program in assembly to figure out how a calculator works internally, but bitwise operations are typically explored in these lesson sets
+
+The other operations used in this program are print(), and format().
+print() is used to display the results of the operation to the console.
+format() is used to make it look nice.
+Learn more at: https://docs.python.org/3/tutorial/inputoutput.html
+
+To run the program: 
+    1. Open a terminal on a computer with python 3 installed (works with v2 as well)
+    2. Navigate to a directory containing this file (long_addition_1.py)
+    3. Run the program with the following command: python3 long_addition_1.py
+    4. A print out of the sums should appear after the cursor
+
+If error:
+    a. make sure python is installed (run: python3 --version)
+    b. make sure you have the file installed and in the current directory
+    c. if you are still having issues or see any general issues in the programming please report using the contact information at end of file.
+"""
+
+# Problem 1 
+difference = 674 - 60  
+print("{0:<4} - {1:<4} = {2:<5}".format(674,60,difference)) 
+
+# Problem 2 
+difference = 275 - 94  
+print("{0:<4} - {1:<4} = {2:<5}".format(275,94,difference)) 
+
+# Problem 3 
+difference = 48 - 42  
+print("{0:<4} - {1:<4} = {2:<5}".format(48,42,difference)) 
+
+# Problem 4 
+difference = 331 - 36  
+print("{0:<4} - {1:<4} = {2:<5}".format(331,36,difference)) 
+
+# Problem 5 
+difference = 334 - 54  
+print("{0:<4} - {1:<4} = {2:<5}".format(334,54,difference)) 
+
+# Problem 6 
+difference = 859 - 98  
+print("{0:<4} - {1:<4} = {2:<5}".format(859,98,difference)) 
+
+# Problem 7 
+difference = 222 - 6  
+print("{0:<4} - {1:<4} = {2:<5}".format(222,6,difference)) 
+
+# Problem 8 
+difference = 956 - 15  
+print("{0:<4} - {1:<4} = {2:<5}".format(956,15,difference)) 
+
+# Problem 9 
+difference = 346 - 15  
+print("{0:<4} - {1:<4} = {2:<5}".format(346,15,difference)) 
+
+# Problem 10
+difference = 289 - 94  
+print("{0:<4} - {1:<4} = {2:<5}".format(289,94,difference)) 
+
+# Problem 11
+difference = 866 - 61  
+print("{0:<4} - {1:<4} = {2:<5}".format(866,61,difference)) 
+
+# Problem 12
+difference = 970 - 12  
+print("{0:<4} - {1:<4} = {2:<5}".format(970,12,difference)) 
+
+# Problem 13
+difference = 892 - 72  
+print("{0:<4} - {1:<4} = {2:<5}".format(892,72,difference)) 
+
+# Problem 14
+difference = 668 - 32  
+print("{0:<4} - {1:<4} = {2:<5}".format(668,32,difference)) 
+
+# Problem 15
+difference = 344 - 34  
+print("{0:<4} - {1:<4} = {2:<5}".format(344,34,difference)) 
+
+# Problem 16
+difference = 835 - 29  
+print("{0:<4} - {1:<4} = {2:<5}".format(835,29,difference)) 
+
+# Problem 17
+difference = 295 - 55  
+print("{0:<4} - {1:<4} = {2:<5}".format(295,55,difference)) 
+
+# Problem 18
+difference = 582 - 95  
+print("{0:<4} - {1:<4} = {2:<5}".format(582,95,difference)) 
+
+# Problem 19
+difference = 924 - 40  
+print("{0:<4} - {1:<4} = {2:<5}".format(924,40,difference)) 
+
+# Problem 20
+difference = 78 - 66  
+print("{0:<4} - {1:<4} = {2:<5}".format(78,66,difference)) 
+
+# Problem 21
+difference = 402 - 66  
+print("{0:<4} - {1:<4} = {2:<5}".format(402,66,difference)) 
+
+# Problem 22
+difference = 571 - 99  
+print("{0:<4} - {1:<4} = {2:<5}".format(571,99,difference)) 
+
+# Problem 23
+difference = 802 - 70  
+print("{0:<4} - {1:<4} = {2:<5}".format(802,70,difference)) 
+
+# Problem 24
+difference = 934 - 35  
+print("{0:<4} - {1:<4} = {2:<5}".format(934,35,difference)) 
+
+# Problem 25
+difference = 102 - 3  
+print("{0:<4} - {1:<4} = {2:<5}".format(102,3,difference)) 
+
+# Problem 26
+difference = 240 - 77  
+print("{0:<4} - {1:<4} = {2:<5}".format(240,77,difference)) 
+
+# Problem 27
+difference = 176 - 10  
+print("{0:<4} - {1:<4} = {2:<5}".format(176,10,difference)) 
+
+# Problem 28
+difference = 135 - 76  
+print("{0:<4} - {1:<4} = {2:<5}".format(135,76,difference)) 
+
+# Problem 29
+difference = 444 - 94  
+print("{0:<4} - {1:<4} = {2:<5}".format(444,94,difference)) 
+
+# Problem 30
+difference = 430 - 22  
+print("{0:<4} - {1:<4} = {2:<5}".format(430,22,difference)) 
+
+# Problem 31
+difference = 896 - 10  
+print("{0:<4} - {1:<4} = {2:<5}".format(896,10,difference)) 
+
+# Problem 32
+difference = 685 - 45  
+print("{0:<4} - {1:<4} = {2:<5}".format(685,45,difference)) 
+
+# Problem 33
+difference = 655 - 90  
+print("{0:<4} - {1:<4} = {2:<5}".format(655,90,difference)) 
+
+# Problem 34
+difference = 984 - 35  
+print("{0:<4} - {1:<4} = {2:<5}".format(984,35,difference)) 
+
+# Problem 35
+difference = 355 - 80  
+print("{0:<4} - {1:<4} = {2:<5}".format(355,80,difference)) 
+
+"""
+Contact information
+
+General Queries: admin@sophisticatedprimate.com
+Report errors, bugs, and problems at:
+    email:  bug@sophisticatedprimate.com
+    github: https://github.com/sophisticatedprimate/long_addition_1/issues
+"""
